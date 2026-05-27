fdc2a012bac06ea9a10b09fd16e1fb0cd0bf8bbd
Origami-Tobiichi <cyberdark454@gmail.com>
2026-05-27 08:23:42 +0800

    Delete src/bots directory

--- src/bots/seleniumBot.ts
+++
@@ -1,3 +0,0 @@
-export async function runSeleniumBot(url: string): Promise<{ success: boolean; message: string }> {
-  return { success: false, message: 'Selenium not supported on Vercel. Use Puppeteer instead.' };
-}
